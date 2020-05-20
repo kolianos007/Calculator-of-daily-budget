5635a3ae189b4d43d82a437b42f5753c8dab235f
Kolya <kolianos007@gmail.com>
2020-05-20 21:30:46 +0300

    Правильное первое задание

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let money = prompt('Ваш бюджет на месяц?'),
     time = prompt('Введите дату в формате YYYY-MM-DD')
 
-let answer1 = prompt('Введите обязательную статью расходов в этом месяце'),
-    answer2 = prompt('Во сколько обойдется?')
-
 const appData = {
-  money: money,
+  budget: money,
   timeData: time,
-  expences: {
-    answer1: answer2
-  },
-  optionalExpenses: {
-
-  },
+  expences: {},
+  optionalExpenses: {},
   income: [],
   savings: false
 }
 
-alert(money / 30)
+let answer1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    answer2 = prompt('Во сколько обойдется?'),
+    answer3 = prompt('Введите обязательную статью расходов в этом месяце'),
+    answer4 = prompt('Во сколько обойдется?')
+
+appData.expences[answer1] = answer2
+appData.expences[answer3] = answer4
+
+
+alert(appData.budget / 30)
